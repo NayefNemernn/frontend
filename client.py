1effbdd4496d410c1f7bfebcd3849ef7e4c2d275
aBBasNMr <nayefnemernn@gmail.com>
2024-07-06 23:07:42 +0300

    update

--- client.py
+++ client.py
@@ -2,8 +2,8 @@ import os
 import yaml
 
 def load_config():
-    with open('../config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
+    with open(os.path.join(os.path.dirname(__file__), '../config.yaml'), 'r') as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
     return config
 config = load_config
 print(f'Server IP Address: {config['ServerIPAddress']}')
